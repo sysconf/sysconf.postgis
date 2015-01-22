10d8b5ff111ebb5e684a9b2577207d471332e4aa
okapi <jf@geonef.fr>
2015-01-22 01:34:47 +0100

    postgis_webviewer_php/config.inc.php

--- tree/var/lib/postgis_webviewer_php/config.inc.php
+++ tree/var/lib/postgis_webviewer_php/config.inc.php
@@ -1,13 +1,13 @@
 <?php $conn = array(
-	'dbname' => 'test',
+	'dbname' => 'postgis',
 	'host' => 'localhost',
 	'port' => '5432',
-	'user' => 'testuser',
-	'password' => 'abc123',
+	'user' => 'postgres',
+	'password' => 'postgres',
 	'options' => ''
 );
 /** use this one if you are running PostgreSQL 9.0+ and have an older driver **/
-//$bytea_output_setting = "set bytea_output='escape';" 
+//$bytea_output_setting = "set bytea_output='escape';"
 /** use database default **/
-$bytea_output_setting = ""; 
+$bytea_output_setting = "";
 ?>
